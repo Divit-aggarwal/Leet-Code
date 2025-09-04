738939909a61eb8706c8ee17f2d7031a5c2c3cb1
Divit-aggarwal <theagihustler@gmail.com>
2025-09-04 15:57:55 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -0,0 +1,16 @@
+# Last updated: 04/09/2025, 15:57:55
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        temp = head
+        temp1 = head
+        while temp1 and temp1.next:
+            temp= temp.next
+            temp1= temp1.next.next    
+            if temp == temp1 :
+                return True
