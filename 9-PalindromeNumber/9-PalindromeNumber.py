4710d0250c88195843ebcf67f122f98a0889dc41
Divit-aggarwal <theagihustler@gmail.com>
2025-09-04 14:59:25 +0530

    File updated at 04/09/2025, 14:59:25

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,10 +1,10 @@
-# Last updated: 04/09/2025, 14:57:51
+# Last updated: 04/09/2025, 14:59:25
 class Solution(object):
     def isPalindrome(self, x):
+        x=str(x)
+        return True if x == x[::-1] else False
         """
-        :type x: int
+        :type
         :rtype: bool
         """
-        y = str(x)
-        return y == y[::-1]
         
