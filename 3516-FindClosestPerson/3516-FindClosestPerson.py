b2b526656af7c3238845c8c8a2717aa71800eebf
Divit-aggarwal <theagihustler@gmail.com>
2025-09-04 15:17:54 +0530

    File updated at 04/09/2025, 15:17:54

--- 3516-FindClosestPerson/3516-FindClosestPerson.py
+++ 3516-FindClosestPerson/3516-FindClosestPerson.py
@@ -1,10 +1,17 @@
-# Last updated: 04/09/2025, 15:17:12
+# Last updated: 04/09/2025, 15:17:54
 class Solution(object):
     def findClosest(self, x, y, z):
-        if abs(x-z) > abs(y-z) :
+        """
+        :type x: int
+        :type y: int
+        :type z: int
+        :rtype: int
+        """
+        l1=abs(x-z)
+        l2=abs(y-z)
+        if(l1>l2):
             return 2
-        elif abs(y-z) > abs(x-z) :
+        elif(l1<l2):
             return 1
         else:
             return 0
-        
