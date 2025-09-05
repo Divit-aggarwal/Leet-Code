cca517f5ef72851a170a6875cd56d720afd68533
Divit-aggarwal <theagihustler@gmail.com>
2025-09-05 19:29:36 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -0,0 +1,8 @@
+# Last updated: 05/09/2025, 19:29:35
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
