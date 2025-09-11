63ab930a640206b26d9fbc58be1064b5db43ab08
Divit-aggarwal <theagihustler@gmail.com>
2025-09-11 15:58:03 +0530

    Create 2-AddTwoNumbers

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -0,0 +1,32 @@
+// Last updated: 11/09/2025, 15:58:03
+#include<stack>
+using namespace std;
+#include<string>
+class Solution {
+public:
+    bool isValid(string s) {
+               stack<char> s1;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                s1.push(ch);
+            } else {
+                if (s1.empty()) {
+                    return false;
+                }
+                char top = s1.top();
+                s1.pop();
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+                
+                if (ch == ']' && top != '['){
+                 return false;
+                }
+                if (ch == '}' && top != '{'){
+                     return false;
+                }
+            }
+        }
+        return s1.empty();
+    }
+};
