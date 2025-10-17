6d89e65aa8dee91138eb5007db4f103aa49e78a0
Divit-aggarwal <theagihustler@gmail.com>
2025-10-18 00:57:34 +0530

    File updated at 18/10/2025, 00:57:34

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,24 +1,21 @@
-// Last updated: 18/10/2025, 00:45:01
+// Last updated: 18/10/2025, 00:57:34
 class Solution {
 public:
-    int bitwiseComplement(int n) {
-        int bin = 0 ;
-        int y = ~n;
-        int mask_max  = 0 ;
-        int mask_min  = 0 ;
-        int count = 0 ;
-        int x = n ;
-        while ( x != 0 ){
-            x = x >> 1 ;
-            count ++; 
+    bool isPowerOfTwo(int n) {
+        if  (n <=0 ){
+            return false;
         }
-        mask_max = (1 << count) - 1 ;
-        mask_min = -(1 << count)  ;
-        if ( n > 0 )
-        return y&mask_max;
-        else if( n == 0 )
-            return 1 ;
-        else 
-            return y&mask_min;
-        }
+        else{
+        while(n>1){
+            if ( n % 2 == 0){
+                n=n/2.0;
+            }
+            else{
+                return false;
+
+            }
+}
+return true;
+    }
+    }
 };
