29f0290c1677256f678f2b9ce779032c935f1124
Divit-aggarwal <theagihustler@gmail.com>
2025-10-18 00:45:02 +0530

    File updated at 18/10/2025, 00:45:01

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,25 +1,24 @@
-// Last updated: 18/10/2025, 00:13:37
-#include <iostream>
-using namespace std;
-
+// Last updated: 18/10/2025, 00:45:01
 class Solution {
 public:
-    int reverse(int x) {
-            int rem = 0 ;
-            long int rev = 0 ;
-            while ( x !=0 ){
-                rem = x%10;
-                rev = rev*10 + rem ;
-                x=x/10;
-            }
-            if ((rev > INT_MAX) || (rev < INT_MIN)){
-                            return 0;
-            }
-            else{
-                return rev;
-            }
-       
+    int bitwiseComplement(int n) {
+        int bin = 0 ;
+        int y = ~n;
+        int mask_max  = 0 ;
+        int mask_min  = 0 ;
+        int count = 0 ;
+        int x = n ;
+        while ( x != 0 ){
+            x = x >> 1 ;
+            count ++; 
         }
-        
-    
+        mask_max = (1 << count) - 1 ;
+        mask_min = -(1 << count)  ;
+        if ( n > 0 )
+        return y&mask_max;
+        else if( n == 0 )
+            return 1 ;
+        else 
+            return y&mask_min;
+        }
 };
