da134695aece7cf3caf309c64a0f339622dc150d
Divit-aggarwal <theagihustler@gmail.com>
2025-10-27 23:54:23 +0530

    File updated at 27/10/2025, 23:54:23

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.cpp
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.cpp
@@ -1,22 +1,17 @@
-// Last updated: 27/10/2025, 23:39:03
+// Last updated: 27/10/2025, 23:54:23
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> freq;
+        unordered_map<int , int > freq;
 
-        for(auto val: arr)
-        {
+        for ( auto val : arr ){
             freq[val]++;
         }
-
-        unordered_set<int> st;
-
-        for(auto val: freq)
-        {
-            if(st.count(val.second)) return false;
-            st.insert(val.second);
-        }
-
-        return true;
+        unordered_set<int> set ;
+     for ( auto val : freq ){
+        if (set.count(val.second)) return false;
+        set.insert(val.second);
+    }
+    return true;
     }
 };
