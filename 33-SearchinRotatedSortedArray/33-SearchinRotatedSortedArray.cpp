ea02f079f5bc7d6c20a9b126a6b3a05f612a93d1
Divit-aggarwal <theagihustler@gmail.com>
2025-11-27 17:38:28 +0530

    File updated at 27/11/2025, 17:38:28

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,45 +1,27 @@
-// Last updated: 27/11/2025, 16:55:29
+// Last updated: 27/11/2025, 17:38:28
 1class Solution {
 2public:
-3    int search(vector<int>& nums, int target) {
-4        int high = nums.size() -1 ;
-5        int low = 0 ;
-6        int mid = (high + low )/2;
-7        while(high>low){    
-8            if(nums[mid]>nums[high]){
-9                low = mid + 1 ;
-10            }
-11            else{
-12                high = mid ;
-13            }
-14            mid = (high + low )/2;
-15        }
-16       int p = low ;
-17        low = 0;
-18         high = nums.size() - 1 ;
-19       if ( target == nums[p]){
-20        return p;
-21       }
-22       else if(target >= nums[p] && target <= nums.back() ) {
-23        low = p ;
-24       }
-25       else{
-26        high = p -1 ;
-27       }
-28
-29        while ( high >= low){
-30            mid = (high + low)/2;
-31            if(target == nums[mid]){
-32                return mid ;
-33            }
-34            else if (target < nums[mid]){
-35                high = mid -1;
-36            }
-37            else{
-38                low = mid + 1;
-39            }
-40        }
-41    
-42      return -1;
-43    }
-44};
+3    int mySqrt(int x) {
+4        unsigned long long high = x;
+5        unsigned long long low = 0; 
+6        unsigned long long mid ;
+7        unsigned long long y = 0 ;
+8        unsigned long long an=0;
+9        while ( high >= low){
+10            mid = (high + low)/2;
+11             y = mid*mid;
+12            if ( y == x ){
+13                return mid;
+14            }
+15            if (y < x){
+16                 an = mid ;
+17                low = mid + 1;
+18            }
+19            else{
+20                high = mid - 1;
+21            }
+22
+23        }
+24        return an;
+25    } 
+26};
