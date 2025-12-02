1172e06899e10227adb216badfcd8367d2332a41
Divit-aggarwal <theagihustler@gmail.com>
2025-12-02 16:59:53 +0530

    File updated at 02/12/2025, 16:59:53

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,32 +1,52 @@
-// Last updated: 02/12/2025, 16:59:34
-1class Solution {
-2public:
-3    bool checkInclusion(string s1, string s2) {
-4        bool f;
-5        if (s2.find(s1) > -1) {
-6            return true;
-7        }
-8        sort(s1.begin(), s1.end());
-9        string temp;
-10        int x = s1.length();
-11        if (x > s2.length()) {
-12            return false;
-13        }
-14        for (int i = 0; i < s2.length(); i++) {
-15            int count = 0;
-16            temp.push_back(s2[i]);
-17            if (temp.length() > x) {
-18                temp.erase(0, 1);
-19            }
-20            if(temp.length()==x){
-21            string temp2 = temp;
-22            sort(temp2.begin(), temp2.end());
-23            if (temp2 == s1) {
-24                f = true;
-25                return true;
-26            }
-27        }
-28        }
-29        return false;
-30    }
-31};
+// Last updated: 02/12/2025, 16:59:53
+class Solution {
+public:
+bool check(int count1[26],int count2[26]){
+ for(int i=0;i<26;i++){
+    if(count1[i] !=count2[i]){
+        return 0;
+    }
+ }
+ return 1;
+}
+    bool checkInclusion(string s1, string s2) {
+    
+       int count1[26]= {0};
+       int index=0;
+       for(int i=0;i<s1.length();i++){
+        index = s1[i]-'a';
+        count1[index]++;
+         }
+
+         int count2[26]={0};
+        int i=0;
+        int windowsize = s1.length();
+
+        while(i<windowsize && i<s2.length()){
+            index = s2[i]-'a';
+            count2[index]++;
+            i++;
+        }
+        if(check(count1,count2)){
+            return 1;
+        }
+
+        while(i<s2.length()){
+          char newchar = s2[i];
+         int index = newchar-'a';
+               count2[index]++;
+             char oldchar = s2[i-windowsize];
+            index = oldchar-'a';
+        count2[index]--;
+
+     if(check(count1,count2)){
+    return 1;
+    }
+      i++;
+        }
+
+return 0;
+
+     
+    }
+};
