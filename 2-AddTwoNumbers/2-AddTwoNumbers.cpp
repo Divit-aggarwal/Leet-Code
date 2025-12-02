ed57f61da6497f8f8d3c4fccf11675dbc9e20b9d
Divit-aggarwal <theagihustler@gmail.com>
2025-12-02 16:59:34 +0530

    File updated at 02/12/2025, 16:59:34

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,10 +1,32 @@
-// Last updated: 02/12/2025, 16:02:16
+// Last updated: 02/12/2025, 16:59:34
 1class Solution {
 2public:
-3    string removeOccurrences(string s, string part) {
-4       while(s.length()>0 && s.find(part) < s.length() ){
-5            s.erase(s.find(part) , part.length());
-6       }
-7       return s;
-8    }
-9};
+3    bool checkInclusion(string s1, string s2) {
+4        bool f;
+5        if (s2.find(s1) > -1) {
+6            return true;
+7        }
+8        sort(s1.begin(), s1.end());
+9        string temp;
+10        int x = s1.length();
+11        if (x > s2.length()) {
+12            return false;
+13        }
+14        for (int i = 0; i < s2.length(); i++) {
+15            int count = 0;
+16            temp.push_back(s2[i]);
+17            if (temp.length() > x) {
+18                temp.erase(0, 1);
+19            }
+20            if(temp.length()==x){
+21            string temp2 = temp;
+22            sort(temp2.begin(), temp2.end());
+23            if (temp2 == s1) {
+24                f = true;
+25                return true;
+26            }
+27        }
+28        }
+29        return false;
+30    }
+31};
