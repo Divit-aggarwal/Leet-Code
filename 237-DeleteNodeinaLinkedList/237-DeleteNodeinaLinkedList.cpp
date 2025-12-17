5be6c8308482d6bde53ef516748cb952257f934e
Divit-aggarwal <theagihustler@gmail.com>
2025-12-17 23:20:45 +0530

    File updated at 17/12/2025, 23:20:45

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
@@ -1,24 +1,31 @@
-// Last updated: 17/12/2025, 23:16:16
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode(int x) : val(x), next(NULL) {}
-7 * };
-8 */
-9class Solution {
-10public:
-11    void deleteNode(ListNode* node) {
-12        ListNode* temp = node;
-13        ListNode* temp2 = node;
-14        while(temp -> next!=nullptr){
-15            if( temp ->next->next ==nullptr){
-16                temp2=temp;
-17            }
-18            temp -> val = temp -> next -> val;
-19            temp = temp -> next;
-20        }
-21         temp2->next =nullptr;
-22    }
-23};
+// Last updated: 17/12/2025, 23:20:45
+1class Solution {
+2public:
+3    ListNode* deleteDuplicates(ListNode* head) {
+4        ListNode dummy(0);
+5        dummy.next = head;
+6
+7        ListNode* prev = &dummy;
+8        ListNode* curr = head;
+9
+10        while (curr != nullptr) {
+11            
+12            if (curr->next != nullptr && curr->val == curr->next->val) {
+13                int dupVal = curr->val;
+14
+15            
+16                while (curr != nullptr && curr->val == dupVal) {
+17                    curr = curr->next;
+18                }
+19
+20                prev->next = curr;  
+21            } 
+22            else {
+23                prev = curr;  
+24                curr = curr->next;
+25            }
+26        }
+27        return dummy.next;
+28    }
+29};
+30
