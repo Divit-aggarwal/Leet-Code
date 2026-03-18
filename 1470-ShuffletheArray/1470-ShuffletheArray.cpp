96cc698db1b67131d0b3ba1381753612c40f0c91
Divit-aggarwal <theagihustler@gmail.com>
2026-03-18 11:05:28 +0530

    File updated at 18/03/2026, 11:05:27

--- 1470-ShuffletheArray/1470-ShuffletheArray.cpp
+++ 1470-ShuffletheArray/1470-ShuffletheArray.cpp
@@ -1,12 +1,24 @@
-// Last updated: 18/03/2026, 10:53:23
+// Last updated: 18/03/2026, 11:05:27
 1class Solution {
 2public:
-3    vector<int> shuffle(vector<int>& nums, int n) {
-4        vector<int> x ; 
-5        for ( int i = 0 ; i<n; i+=1){
-6            x.push_back(nums[i]);
-7            x.push_back(nums[i + n]);
-8        }
-9        return x;
-10    }
-11};
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int Hmax = 0;
+5        int Lmax = 0;
+6        for (int i = 0; i < nums.size(); i++) {
+7            if (nums[i] == 1) {
+8                Lmax++;
+9            }
+10
+11            if (nums[i] == 0) {
+12                if (Lmax > Hmax) {
+13                    Hmax = Lmax;
+14                }
+15                Lmax = 0;
+16            }
+17            if (Lmax > Hmax) {
+18                Hmax = Lmax;
+19            }
+20        }
+21        return Hmax;
+22    }
+23};
