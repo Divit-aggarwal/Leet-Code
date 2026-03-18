3573bc649c75071fa0d345906713436c0e518775
Divit-aggarwal <theagihustler@gmail.com>
2026-03-18 11:36:14 +0530

    File updated at 18/03/2026, 11:36:14

--- 1470-ShuffletheArray/1470-ShuffletheArray.cpp
+++ 1470-ShuffletheArray/1470-ShuffletheArray.cpp
@@ -1,24 +1,29 @@
-// Last updated: 18/03/2026, 11:05:27
+// Last updated: 18/03/2026, 11:36:14
 1class Solution {
 2public:
-3    int findMaxConsecutiveOnes(vector<int>& nums) {
-4        int Hmax = 0;
-5        int Lmax = 0;
-6        for (int i = 0; i < nums.size(); i++) {
-7            if (nums[i] == 1) {
-8                Lmax++;
-9            }
-10
-11            if (nums[i] == 0) {
-12                if (Lmax > Hmax) {
-13                    Hmax = Lmax;
-14                }
-15                Lmax = 0;
+3    vector<int> findErrorNums(vector<int>& nums) {
+4        unordered_map<int, int> map;
+5        vector<int> q;
+6        int s = 0;
+7        int e = nums.size();
+8        int ne = e * (e + 1) / 2;
+9
+10        for (int i : nums) {
+11            s = s + i;
+12            if (map.count(i)) {
+13                map[i]++;
+14            } else {
+15                map[i] = 1;
 16            }
-17            if (Lmax > Hmax) {
-18                Hmax = Lmax;
-19            }
-20        }
-21        return Hmax;
-22    }
-23};
+17        }
+18        for (const auto& x : map) {
+19            if (x.second > 1) {
+20                q.push_back(x.first);
+21                int f = s - ne - x.first;
+22                q.push_back(abs(f));
+23                break;
+24            }
+25        }
+26        return q;
+27    }
+28};
